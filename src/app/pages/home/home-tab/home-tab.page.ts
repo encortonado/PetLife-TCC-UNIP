f951eb6ad267fa9d562cdfc65ea96e928b040c9c
encortonado <lucasbortolo3@gmail.com>
2022-02-02 16:06:29 -0300

    home criado

--- src/app/pages/home/home-tab/home-tab.page.ts
+++ src/app/pages/home/home-tab/home-tab.page.ts
@@ -1,4 +1,10 @@
+import { User } from 'src/app/models/user';
+import { Ads } from './../../../models/ads';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AdsService } from './../../../services/ads/ads.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/authentication/user/user.service';
 
 @Component({
   selector: 'app-home-tab',
@@ -7,9 +13,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTabPage implements OnInit {
 
-  constructor() { }
+  anuncios: Ads[];
+  usuario: User;
+  cidades: string[];
+  cidade = '';
+  servico = '';
+
+  constructor(
+    private router: Router,
+    private adsService: AdsService,
+    public userService: UserService,
+    public alertController: AlertController,
+    public loadingController: LoadingController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+
+    console.log('Cidade e servico' + this.servico + this.cidade);
+
+    if (!(this.cidade === 'Todos' && this.servico === '' || this.servico === 'Todos' && this.cidade === '')) {
+
+      if (this.cidade === '' && this.servico === '') {
+        this.adsService.findAll().subscribe((response) => {
+          this.anuncios = response;
+          console.log(this.anuncios);
+        },
+          error => {
+            console.log(error);
+          });
+      } else {
+
+        console.log('Entrou no filtro ' + this.servico + this.cidade);
+        this.adsService.findAllWithParameters(this.cidade, this.servico).subscribe(res => {
+          this.anuncios = res;
+          console.log(this.anuncios);
+        },
+          error => {
+            console.log(error);
+          });
+
+      }
+    }
+
+    this.userService.findAllCities().subscribe(res => {
+      this.cidades = res;
+      console.log(this.cidades);
+    });
+
+  }
+
+  viewAnuncio(anuncio: Ads) {
+    console.log(anuncio);
+    this.router.navigate(['/ad'], {
+      queryParams: anuncio
+    });
+  }
+
+  viewProfile(userId: string) {
+
+    console.log(userId);
+    this.userService.findUserById(userId).subscribe(res => {
+
+      this.usuario = res;
+
+      this.router.navigate(['/profile'], {
+        queryParams: this.usuario
+      });
+    });
+  }
+
+  changeSelectedCity(event) {
+    this.cidade = event.detail.value;
+    console.log(event.detail.value);
+    this.ionViewWillEnter();
+  }
+
+  changeSelectedService(event) {
+    this.servico = event.detail.value;
+    console.log(event.detail.value);
+    this.ionViewWillEnter();
   }
 
 }
